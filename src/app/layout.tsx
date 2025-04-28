cfda77eb837ccd4a4ff7150172c5ef45e0b80551
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2025-04-28 15:44:11 +0700

    feat: add auto logout (#10)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NuqsProvider from "@/providers/NuqsProvider";
+import TokenProvider from "@/providers/TokenProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,8 +34,10 @@ export default function RootLayout({
       >
         <ReactQueryProvider>
           <NuqsProvider>
-            <Navbar />
-            {children}
+            <TokenProvider>
+              <Navbar />
+              {children}
+            </TokenProvider>
           </NuqsProvider>
         </ReactQueryProvider>
 
